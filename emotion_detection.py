b1d06a1865a7f1bcbedfb344bffea628a22354a2
Hasansarwer <hasanmathju@gmail.com>
2024-12-23 02:44:40 +0600

    array changes for test

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,7 +7,8 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    emotion = formatted_response['emotionPredictions']['emotion']
+    emotion = formatted_response['emotionPredictions']
+    print(emotion)
     dominant_emotion = max(emotion, key=emotion.get)
     emotion["dominant_emotion"] = dominant_emotion
     return {
