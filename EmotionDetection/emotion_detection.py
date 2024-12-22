113ff7aa62d412642b29a8bdbacc585857322dd2
Hasansarwer <hasanmathju@gmail.com>
2024-12-23 04:41:11 +0600

    correction 2

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,17 +7,19 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    emotion = formatted_response['emotionPredictions'][0]["emotion"]
     if response.status_code == 200:
+        emotion = formatted_response['emotionPredictions'][0]["emotion"]
         dominant_emotion = max(emotion, key=emotion.get)
         emotion["dominant_emotion"] = dominant_emotion
     elif response.status_code == 400:
-        emotion["anger"] = "None"
-        emotion["disgust"] = "None"
-        emotion["fear"] = "None"
-        emotion["joy"] = "None"
-        emotion["sadness"] = "None"
-        emotion["dominant_emotion"] = "None"
+        emotion = {
+            "anger": "None",
+            "disgust": "None",
+            "fear": "None",
+            "joy": "None",
+            "sadness": "None",
+            "dominant_emotion": "None"
+        }
     return {
         'anger': emotion["anger"],
         'disgust': emotion["disgust"],
