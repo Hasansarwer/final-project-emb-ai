0964ea92a0b27ba5778d3934e2cc2f91a6515077
Hasansarwer <hasanmathju@gmail.com>
2024-12-23 04:03:54 +0600

    index page added

--- server.py
+++ server.py
@@ -8,3 +8,10 @@ def sent_emotion():
     text_to_analyse = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyse)
     return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(response["anger"], response["disgust"], response["fear"], response["joy"], response["sadness"], response["dominant_emotion"])
+
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
